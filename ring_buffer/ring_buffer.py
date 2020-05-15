52cbc885ef3d06c8d11757576ef053b5e6d4414e
ethanmjansen <ethanmjansen@users.noreply.github.com>
2020-05-15 11:08:56 -0600

    MVP Ring Buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,36 @@
+from collections import deque
+
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.storage = deque(maxlen=self.capacity)
+        self.neg_count = 1 # Part 1 of my rotating pointer
+        self.pos_count = 0 # Part 2 of my rotating pointer
 
     def append(self, item):
-        pass
+        # Reset both parts of the pointer when it's time
+        if self.capacity == (self.neg_count - 1):
+            self.neg_count = 1
+            self.pos_count = 0 
+
+        # Ring buffer is not full
+        if len(self.storage) < self.capacity:
+            self.storage.append(item)
+
+        # Ring buffer is full
+        elif self.capacity == len(self.storage):
+            self.storage.rotate(self.capacity - self.neg_count)
+            self.storage.appendleft(item)
+            self.storage.rotate(self.capacity + self.pos_count)
+            self.neg_count = self.neg_count + 1
+            self.pos_count = self.pos_count + 1
 
     def get(self):
-        pass
+        return list(self.storage)
+
+if __name__ == '__main__':
+    # A little testy test
+    ring = RingBuffer(5)
+    for i in range(50):
+        ring.append(i)
+        print(ring.get())
